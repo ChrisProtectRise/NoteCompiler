a841d52c568a80cedb9aaa379aa3603bb9a3e8d8
= <=>
2026-02-01 18:30:26 -0800

    Use large model for whisper

--- transcription.py
+++ transcription.py
@@ -2,6 +2,11 @@ import whisper
 
 
 def transcribe_wav(filename: str) -> str:
-    model = whisper.load_model("base")  # tiny / base / small / medium / large
+    model = whisper.load_model("large")  # tiny / base / small / medium / large
     result = model.transcribe(filename, language = "en", fp16 = False)
     return result["text"]
+
+
+if __name__ == "__main__":
+    transcribed_text = transcribe_wav("recording-2026-01-25_15-29-21.wav")
+    print(transcribed_text)
